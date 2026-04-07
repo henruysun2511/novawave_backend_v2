9a2282f7e672d69806761ae7fd6efebbb69c5af5
huysun <huysun2511@gmail.com>
2026-04-07 16:51:15 +0700

    add domain fe

--- src/common/constants/whitelist.constant.ts
+++ src/common/constants/whitelist.constant.ts
@@ -1,1 +1,1 @@
-export const whitelist_domain_cors = ['http://localhost:3001', 'https://novawave.vercel.app', 'https://novawave-music-streaming.vercel.app/'];
+export const whitelist_domain_cors = ['http://localhost:3001', 'https://novawave.vercel.app', 'https://novawave-music-streaming.vercel.app'];
